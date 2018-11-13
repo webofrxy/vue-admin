cb608567838f1bf0e417b6c946bda584f50a5589
renxy <renxy@hzanchu.com>
2018-11-13 21:02:19 +0800

    搭建路由和布局

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Element from 'element-ui';
+import ECharts from 'vue-echarts/components/ECharts'
+import 'element-ui/lib/theme-chalk/index.css'; 
+import axios from 'axios'
+
+Vue.use(Element);
+// Vue.component('v-chart', ECharts)
+Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import { resolve } from 'url';
+
+const Home = ()=> import('@/pages/Home');
+const Dashboard = ()=> import('@/pages/Dashboard');
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path:'/',
+      redirect:'/dashboard',
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home,
+      meta:{title:'首页'},
+      children:[
+        {
+          path:'/dashboard',
+          component: Dashboard,
+          meta:{title: '控制面板'}
+        }
+      ]
     }
   ]
 })
